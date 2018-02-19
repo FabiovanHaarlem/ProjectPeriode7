0364755e8f671dbf28d52572fea3742e7fd99b64
Fabio van Haarlem <fabio.van.haarlem@hotmail.nl>
2018-02-19 08:39:09 +0100

    Started on middle music notes

---
+++ Project7/Assets/Scripts/Fabio/MiddleMusicNote.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleMusicNote : MonoBehaviour
+{
+    public void Setup(Vector2 position)
+    {
+        transform.position = position;
+        gameObject.SetActive(true);
+    }
+
+    public void ResetMusicNote()
+    {
+        gameObject.SetActive(false);
+        //Change sprite back to normale or disable particle
+    }
+
+    public void Found()
+    {
+        //Change sprite to found or activate particle
+    }
+}
