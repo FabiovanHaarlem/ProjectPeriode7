ce86b62813d01b77e908732a7bcc9f911215504f
Suzanne <suzannedistelbrink@hotmail.com>
2018-03-05 10:41:41 +0100

    pause menu af!

--- Project7/Assets/Scripts/Suzanne/Pause.cs
+++ Project7/Assets/Scripts/Suzanne/Pause.cs
@@ -8,24 +8,42 @@ public class Pause : MonoBehaviour
     private float s_TimerStartPausePressed;
     private float s_StartTimer;
     [SerializeField] private GameObject s_Text;
+    [SerializeField] private PauseMenu s_Menu;
+    public bool s_IsPaused;
 
-    private void Update()
+    private void LateUpdate()
     {
-        Debug.Log(s_TimerStartPausePressed);
+        Debug.Log("uhm : " + s_TimerStartPausePressed);
+        Debug.Log("timer : " + s_StartTimer);
+
+
+        //check if button is pushed for atleast 5 seconds.
+        if (s_StartTimer > 5)
+        {
+            s_IsPaused = true;
+            s_Text.SetActive(true);
+            s_TimerStartPausePressed = 0;
+            s_StartTimer = 0;
+        }
+        if(s_IsPaused == false)
+        {
+            s_Text.SetActive(false);
+        }
+
     }
     public void OnMouseDown()
     {
-        s_TimerStartPausePressed = Time.time;
-        if (s_StartTimer > 5)
+        if (s_IsPaused == false)
         {
-            s_Text.SetActive(true);
-            Debug.Log("yaay");
-            s_TimerStartPausePressed = 5;
+            s_TimerStartPausePressed = Time.time;
         }
     }
     public void OnMouseUp()
     {
-        s_StartTimer = Time.time - s_TimerStartPausePressed;
+        if (s_IsPaused == false)
+        {
+            s_StartTimer = Time.time - s_TimerStartPausePressed;
+        }
     }
 
 }
---
+++ Project7/Assets/Scripts/Suzanne/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField]private Pause s_Bool;
+    [SerializeField] private List<GameObject> s_Buttons = new List<GameObject>();
+    [SerializeField] private GameObject s_Main;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(s_Bool.s_IsPaused)
+        {
+            Time.timeScale = 0;
+            s_Main.SetActive(true);
+        }
+	}
+    public void Resume()
+    {
+        s_Bool.s_IsPaused = false;
+        if(s_Bool.s_IsPaused == false)
+        {
+            Time.timeScale = 1;
+            foreach (GameObject buttons in s_Buttons)
+            {
+                buttons.SetActive(false);
+            }
+        }
+    }
+    public void Options()
+    {
+        foreach (GameObject buttons in s_Buttons)
+        {
+            buttons.SetActive(false);
+        }
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
